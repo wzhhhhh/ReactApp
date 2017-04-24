87e3012ff7821dd9a8bd12a63df586900650afe0
zelito1 <253639391@qq.com>
2017-04-24 20:16:51 +0800

    第一次修改完

---
+++ src/script/components/mine/onlogin/onlogin.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class OnloginHeader extends React.Component {
+	render (){
+		return(
+			<div className="onloginheader">
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+			)
+	}
+}
