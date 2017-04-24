031339c2a079c4b174b55953b7b413f72a14281b
zelito1 <253639391@qq.com>
2017-04-24 19:29:35 +0800

    denglu

--- src/script/app.js
+++ src/script/app.js
@@ -9,3 +9,14 @@ ReactDOM.render (
 	<Login />,
 	document.getElementById('root')
 	)
+
+// window.onload = function(){
+	var plu = document.getElementsByClassName('loginall');
+	console.log(1);
+	plu.onmousemove = function(){
+			console.log(109);
+			plu.style.left = 0;
+	}
+	
+	
+// }
