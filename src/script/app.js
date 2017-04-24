fcea40878237f0cc8171bc6b58ca1b1b0d87211a
wzhhhhh <13161987838@163.com>
2017-04-24 22:26:03 +0800

    car1

--- src/script/app.js
+++ src/script/app.js
@@ -8,7 +8,7 @@ import Home from './components/home/component'
 import Banner from './components/home/module/banner'
 import Classify from './components/classify/classify'
 import EyeShot from './components/eye/eyeShot'
-import Cart from './components/car/Cart'
+import CartIndex from './components/car/CartIndex'
 import Login from './components/mine/login'
 import {Router, Route, hashHistory, browserHistory, IndexRedirect} from 'react-router'
 
@@ -19,7 +19,7 @@ ReactDOM.render((
 			<route path="home" component={Home}/>
 			<route path="classify" component={Classify}/>
 			<route path="eyeShot" component={EyeShot}/>
-			<route path="cart" component={Cart}/>
+			<route path="cart" component={CartIndex}/>
 			<route path="mine" component={Login}/>
 		</Route>
 	</Router>	
