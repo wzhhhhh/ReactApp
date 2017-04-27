4b6e5e44fea2b8ec0b3a23494f626da36ae79d63
zhanglibao <11454332@qq.com>
2017-04-27 08:39:14 +0800

    tianjia list

--- src/script/components/classify/classifyList/classifylist.js
+++ src/script/components/classify/classifyList/classifylist.js
@@ -1,10 +1,13 @@
 import React from "react"
+import List from "../../../../component_dev/list/src"
+
 
 class Classifylist extends React.Component{
 	constructor(props) {
 	    super(props)
 	    this.state = {
-	      List: []
+	      List: [<div/>],
+	      page:1
 	    }
 	}
 
@@ -12,16 +15,45 @@ class Classifylist extends React.Component{
 		this.props.gotoDetail(goods_id)
 	}
 	render(){
-		let List = this.state.List.map((value)=>{return (
-                <li onClick={this.gotoDetail.bind(this,value.goods_id)}>
-					<img src={value.list_img} alt=""/>
-					<p>{value.goods_name}</p>
-					<b>{`￥${value.miaohui_price}`}</b>
-				</li>
-            )})
 		return(
 			<div className="clist">
-				{List}
+				<List
+					ref="list"
+					dataSource={this.state.List}
+				    renderItem={(value,i)=>{
+				        return (
+			                <li onClick={this.gotoDetail.bind(this,value.goods_id)}>
+								<img src={value.list_img} alt=""/>
+								<p>{value.goods_name}</p>
+								<b>{`￥${value.miaohui_price}`}</b>
+							</li>
+			            )
+				    }}
+				
+					usePullRefresh={true}
+					onRefresh={() => {
+				        fetch(`/api/getRecommendTipsGoodsList?page=${this.state.page++}&size=10&catID=1&tag=`)
+			    		.then((response)=>response.json())
+			    		.then((res)=>{
+			    			this.setState({
+			    				List:res.list.goods
+			    			})
+			    		})
+				        this.refs.list.stopRefreshing(true); // 这个调用也可以放在异步操作的回调里之后
+				    }}
+
+				    useLoadMore={true}
+				    onLoad={() => {
+				        fetch(`/api/getRecommendTipsGoodsList?page=${this.state.page++}&size=10&catID=1&tag=`)
+			    		.then((response)=>response.json())
+			    		.then((res)=>{
+			    			this.setState({
+			    				List:this.state.List.concat(res.list.goods)
+			    			})
+			    		})
+				        this.refs.list.stopLoading(true); // 这个调用也可以放在异步操作的回调里之后
+				    }}
+				></List>
 			</div>
 			)
 	}
--- src/script/components/home/search.js
+++ src/script/components/home/search.js
@@ -1,12 +1,29 @@
 import React,{Component} from 'react'
-import Suggest from '../../../component_dev/suggest/src';
 
 
 
 import Header from './module/header'
 export default class Search extends Component {
+
 	back(){
 		this.props.router.goBack()
+
+	}
+
+	keypress(){
+		let headers = new Headers({
+		'Content-Type': 'application/json;charset=UTF-8'
+		 // 'Content-Type': 'application/x-www-form-urlencoded'
+		})
+		fetch(`/api/searchList`,{
+			method: 'POST', 
+			headers:headers,
+			body:`page=1&size=10&searchKey=${'电池'}`
+		})
+		.then((response)=>response.json())
+		.then((res)=>{
+			console.log(res)
+		}).catch(e=>console.log(e))
 	}
 	
   	render(){
@@ -16,7 +33,7 @@ export default class Search extends Component {
 	    			<img src="/images/menu_back.png" />
 	    		</div>
 	    		<div className="search-input">
-					<input type="text" /><img src="/images/home_search.png" />
+					<input type="text" ref="text" onBlur={this.keypress.bind(this)}/><img src="/images/home_search.png" />
 	    		</div>
 	    		<div className="search-scan">
 					<img src="/images/home_qrcode_p.png" />
